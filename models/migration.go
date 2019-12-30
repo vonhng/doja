6f64797e8bb74c04a4dbbd9742fc7b0feed88182
Feng Qianyong <vonhng@qq.com>
2019-12-31 00:41:11 +0800

    add mongodb

--- models/migration.go
+++ models/migration.go
@@ -2,3 +2,37 @@
 // create_time: 2019/12/30 - 00:05
 // mail: vonhng.feng@gmail.com
 package models
+
+import (
+	"fmt"
+	"github.com/grafana/grafana/pkg/infra/log"
+	"gopkg.in/mgo.v2"
+	"vonhng/doja/pkg/config"
+)
+
+type DBAuth struct {
+	Username string `json:"username",yaml:"username",valid:"Required; MaxSize(50)"`
+	Password string `json:"password",yaml:"password",valid:"Required; MaxSize(50)"`
+}
+
+func (c *DBAuth) Connect() {
+	ConnectionUrl := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?authMechanism=SCRAM-SHA-1",
+		config.Setting.DB.Username,
+		config.Setting.DB.Password,
+		config.Setting.DB.Host,
+		config.Setting.DB.Port,
+		config.Setting.DB.DBName)
+	session, err := mgo.Dial(ConnectionUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB(config.Setting.DB.DBName).C("test")
+	OneAuth := &DBAuth{}
+	err = c.Find(bson.M{"username": "fqy"}).One(OneAuth)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info(OneAuth.Password)
+}
